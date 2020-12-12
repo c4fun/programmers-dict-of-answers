1136d8fa852a61d9b3466b086b11bc01e23fdb4a
c4fun <pcfeng502@gmail.com>
2020-12-12 12:07:42 +0800

    Improve an answer's text

--- src/res/initializeListOfAnswers.ts
+++ src/res/initializeListOfAnswers.ts
@@ -4,7 +4,7 @@ export function generateAllAnswers() : Answer[] {
     let allAnswers: Answer[] = [
         new Answer(0, "否"),
         new Answer(1, "是"),
-        new Answer(2, "xue ding le")
+        new Answer(2, "自己选的路，跪着也要把它走完")
     ]
     return allAnswers;
 }
